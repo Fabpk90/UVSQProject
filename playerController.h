c5a0f01a398d8cf298080ac319a1fba8e939bf47
Fabpk90 <fabrizio1996@live.fr>
2017-11-25 19:59:13 +0100

    Finally found how to clean the repo

--- playerController.h
+++
@@ -1,6 +0,0 @@
-#ifndef PLAYER
-#define PLAYER
-
-int8_t movePlayer(Slider * slider, POINT direction);
-
-#endif
--- renderer.h
+++
@@ -1,8 +0,0 @@
-#ifndef FICHIER
-#define FICHIER
-
-#include "gridStruct.h"
-
-void drawGame(Slider * slider);
-
-#endif
